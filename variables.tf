a36a69ac195d4d4f440c27d46a0284eb01b45237
Mujeebur Rahman <Mujeebur.Rahman@in.ibm.com>
2022-06-16 20:32:18 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,4 +6,5 @@ variable "ibmcloud_api_key" {
 variable "schematics_workspace_resource_group" {
   description = "The ID of the resource group where you want to provision the workspace."
   type        = string
+  value       = "Default"
 }
